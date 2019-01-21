06411e6bc292e3496c32c5a648f4c712c8317966
Nick Romans <nicholas.romans@gmail.com>
2019-01-21 20:24:56 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -17,25 +17,25 @@ end
 end
 
 # # Build two methods #turn_count and #current_player
-# 
+#
 # #turn_count
 # # board array
 # board = ["grid 1", "grid 2", "grid 3", "grid 4", "grid 5", "grid 6", "grid 7", "grid 8","grid 9"]
-# 
+#
 # # declare a counter at 0
 # counter = 0
-# 
-# #turn_count 
+#
+# #turn_count
 # def turn_count(board)
 #   board.each do |grid|
 #     puts "There are #{board.length} turns played."
 #     counter += 1
 #   end
 # end
-# 
+#
 # # current_player
 # def current_player (board)
-#   if turn_count(board).length% 2 == 1 
+#   if turn_count(board).length% 2 == 1
 #     puts "It is  player X's turn."
 #     elsif turn_count(board).length % 2  == 0
 #     puts "It is player O's turn."
